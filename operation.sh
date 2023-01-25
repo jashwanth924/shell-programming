e810210134299be7c054a5a0693eb43162a06b74
jashwanth924 <123528765+jashwanth924@users.noreply.github.com>
2023-01-25 09:26:10 +0530

    Add files via upload

---
+++ operation.sh
@@ -0,0 +1,12 @@
+echo "enter a number"
+read a
+echo "enter a number"
+read b
+sum=$((a+b))
+diff=$((a-b))
+mul=$((a*b))
+div=$((a/b))
+echo "add :" $sum
+echo "sub :" $diff
+echo "mul :" $mul
+echo "div :" $div
